b25174da2bbee8435ee8ec56a7a6d4c86edd47fa
gael.sola <gaelsola@hotmail.fr>
2022-09-03 21:44:14 +0700

    started parameters module

---
+++ R/params_UI.R
@@ -0,0 +1,15 @@
+
+params_UI <- function(id){
+
+  ## From https://shiny.rstudio.com/articles/modules.html
+  # `NS(id)` returns a namespace function, which was save as `ns` and will
+  # invoke later.
+  ns <- NS(id)
+
+  ## UI elements wrapped in a tagList() function
+  tagList(
+
+
+  ) ## END tagList
+
+} ## END function params_UI()
---
+++ R/params_server.R
@@ -0,0 +1,11 @@
+
+
+params_server <- function(id, rv) {
+  moduleServer(
+    id,
+    ## Below is the module function
+    function(input, output, session) {
+
+    }
+  )
+} ## END function params_server()
