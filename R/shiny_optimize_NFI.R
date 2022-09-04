6c3d086b95fdbf19d2882eae12d64408e6bf4374
gael.sola <gaelsola@hotmail.fr>
2022-09-04 16:44:04 +0700

    added params module to main function

--- R/shiny_optimize_NFI.R
+++ R/shiny_optimize_NFI.R
@@ -48,31 +48,46 @@ shiny_optimize_NFI <- function(...) {
   ## Server #################################################################
   server <- function(input, output, session) {
 
-    ## Initiate reactive values list to be passed between modules
+    ## + Initiate reactive values list to be passed between modules =========
     rv <- reactiveValues(
-      ## CV model related values
       CV_model = reactiveValues(
-        file_path = NULL,
-        sf_aoi = NULL,
+        cv_approach  = NULL,
+        file_path    = NULL,
+        sf_aoi       = NULL,
         rs_avitabile = NULL,
         df_avitabile = NULL,
         cv_avitabile = NULL,
-        rs_santoro = NULL,
-        df_santoro = NULL,
-        cv_santoro = NULL,
-        cv_params = NULL
-      ))
+        rs_santoro   = NULL,
+        df_santoro   = NULL,
+        cv_santoro   = NULL,
+        cv_mixed     = NULL
+        ),
+      params = reactiveValues(
+        cv_params     = NULL,
+        nested_params = NULL,
+        time_params   = NULL,
+        opti_params   = NULL
+        )
+      )
 
-    ## Module server functions
+
+
+    ## + Module server functions ============================================
     home_server("tab_home", rv = rv)
 
     CV_model_server("tab_CV_model", rv = rv)
 
     params_server("tab_params", rv = rv)
 
-    ## Trans modules events
+
+
+    ## + Trans modules events ===============================================
     observeEvent(rv$to_CV_model, {
       updateTabsetPanel(session, "navbar", "CV_model")
+    })
+
+    observeEvent(rv$to_params, {
+      updateTabsetPanel(session, "navbar", "params")
     })
 
   }
