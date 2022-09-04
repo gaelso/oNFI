21812c3802ca5c2790d5828bec67d3f8c50eae71
gael.sola <gaelsola@hotmail.fr>
2022-09-04 16:43:27 +0700

    Initial skeleton for params module

--- R/params_UI.R
+++ R/params_UI.R
@@ -9,6 +9,92 @@ params_UI <- function(id){
   ## UI elements wrapped in a tagList() function
   tagList(
 
+    h3("Forest inventory optimization parameters", alignn = "center"),
+
+    br(),
+
+
+    ## Default message ######################################################
+    div(id = ns("check_approach"), fluidRow(
+
+      wellPanel(
+        p("Make sure you fill in the CV model approach and complete the approach 1
+          if you don't have any CV model to apply before coming to this section",
+          style = "color:red; font-style:italic; text-align:center;")
+      )
+
+    )), ## End check_approach
+
+    ## Parameters interface #################################################
+    shinyjs::hidden(div(id = ns("params_setup"), wellPanel(
+
+      ## Steps 1 and 2 ######################################################
+      fluidRow(
+
+        ## + Step 1: Model parameters =======================================
+        column(width = 6, wellPanel(
+          h4("1. CV model parameters"),
+
+          ## + + IF Approach 1 for CV ---------------------------------------
+          conditionalPanel(
+            rv$CV_model$cv_approach == "a1", ns = ns,
+
+            tableOutput(outputId = ns("CV_a1")),
+
+            br(),
+
+            p("To refine the grid spacing, please provide the estimated forest cover of you AOI in %."),
+
+            numericInput(ns("forest_cover"), "Forest cover in %", value = 50, min = 0, max = 100),
+
+            ), ## END conditionalPanel A1
+
+          ## + + IF Approach 2 for CV ---------------------------------------
+          conditionalPanel(
+            rv$CV_model$cv_approach == "a1", ns = ns,
+
+            )
+
+          )), ## END wellPanel step 1
+
+
+
+        ## + Step 2: Nested plot conditions =================================
+        column(width = 6, wellPanel(
+          h4("2. Nested plot conditions")
+
+          ))
+
+      ), ## End fluidRow
+
+
+
+      ## Steps 3 and 4 ######################################################
+      fluidRow(
+
+        ## + Step 3: Unit times =============================================
+        column(width = 6, wellPanel(
+          h4("3. Unit times")
+        )),
+
+
+        ## + Step 4: Optimization parameters ================================
+        column(width = 6, wellPanel(
+          h4("4. Optimization parameters")
+
+        ))
+      ),
+
+
+
+
+    ))) ## End params_setup
+
+
+
+
+
+
 
   ) ## END tagList
 
--- R/params_server.R
+++ R/params_server.R
@@ -6,6 +6,22 @@ params_server <- function(id, rv) {
     ## Below is the module function
     function(input, output, session) {
 
-    }
+      ## Check if CV model approach selected ################################
+      observe({
+
+        if (is.null(rv$CV_model$cv_approach) | is.null(rv$CV_model$cv_mixed)) {
+          shinyjs::show("check_approach")
+          shinyjs::hide("params_setup")
+        } else {
+          shinyjs::hide("check_approach")
+          shinyjs::show("params_setup")
+        }
+
+      })
+
+      ## box 1: CV model ####################################################
+
+
+    } ## END module function
   )
 } ## END function params_server()
