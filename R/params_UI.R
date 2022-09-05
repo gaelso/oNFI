674fac2497d18b3cb7e916e7db9d335641e9a579
gael.sola <gaelsola@hotmail.fr>
2022-09-05 23:13:11 +0700

    Added UI for params

--- R/params_UI.R
+++ R/params_UI.R
@@ -9,9 +9,19 @@ params_UI <- function(id){
   ## UI elements wrapped in a tagList() function
   tagList(
 
-    h3("Forest inventory optimization parameters", alignn = "center"),
+    h3("Forest inventory optimization parameters", align = "center"),
 
     br(),
+
+    ## !!! For testing only !!!
+    hr(),
+
+    textOutput(outputId = ns("test_approach")),
+
+    tableOutput(outputId = ns("test_CV")),
+
+    hr(),
+
 
 
     ## Default message ######################################################
@@ -36,24 +46,60 @@ params_UI <- function(id){
           h4("1. CV model parameters"),
 
           ## + + IF Approach 1 for CV ---------------------------------------
-          conditionalPanel(
-            rv$CV_model$cv_approach == "a1", ns = ns,
+          shinyjs::hidden(div(id = ns("params_a1"),
+
+            h5(HTML(paste0("Inputs from biomass maps for ", strong("approach 1"), " CV model:"))),
+
+            p("$$CV_{opti} = \\sqrt{CV_{init} \\times \\left( \\frac{ area_{CV_{init}} }{ area_{CV_{opti}} } \\right)^{0.5} }$$ "),
 
             tableOutput(outputId = ns("CV_a1")),
 
-            br(),
+            hr(),
 
-            p("To refine the grid spacing, please provide the estimated forest cover of you AOI in %."),
+            p(HTML(paste0("Based on the spatial data uploaded, the AOI total area
+                          in km", tags$sup(2), " is:"))),
 
-            numericInput(ns("forest_cover"), "Forest cover in %", value = 50, min = 0, max = 100),
+            textOutput(outputId = ns("area_aoi_a1")),
 
-            ), ## END conditionalPanel A1
+
+            )), ## END A1 params
 
           ## + + IF Approach 2 for CV ---------------------------------------
-          conditionalPanel(
-            rv$CV_model$cv_approach == "a1", ns = ns,
+          shinyjs::hidden(div(id = ns("params_a2"),
 
-            )
+            h5(HTML(paste0("Inputs parameters for ", strong("approach 2"), " CV model:"))),
+
+            p("$$CV_{opti} = \\beta_{0} \\times n^{\\beta_{1}} \\times d^{\\beta_{2}} \\times A^{\\beta_{3}}$$"),
+
+            p("With: $n$ the number of subplots, $d$ the distance between subplots and $A$ the subplot size in ha,
+              $\\beta_{0}$, $\\beta_{1}$, $\\beta_{2}$ and $\\beta_{3}$ the model parameters to fill in:"),
+
+            fluidRow(
+              column(6, numericInput(inputId = ns("beta0"), label = "$\\beta_{0}$",
+                                     value = 1.02, min = 0, max = 10, step = 0.001)),
+              column(6, numericInput(inputId = ns("beta1"), label = "$\\beta_{1}$",
+                                     value = -0.15, min = -1, max = 1, step = 0.001)),
+            ),
+            fluidRow(
+              column(6, numericInput(inputId = ns("beta2"), label = "$\\beta_{2}$",
+                                     value = 0.016, min = -0.1, max = 0.1, step = 0.001)),
+              column(6, numericInput(inputId = ns("beta3"), label = "$\\beta_{3}$",
+                                     value = -0.12, min = -1, max = 1, step = 0.001))
+              ),
+
+            p(HTML(paste0("For the total costs, fill in the area of interest total area
+                          in km", tags$sup(2), ":"))),
+
+            numericInput(inputId = ns("aoi_area_a2"), label = "", value = 0)
+
+            )), ## END A2 params
+
+          hr(),
+
+          ## + + Collect forest cover percentage in any case ----------------
+          p("To refine the grid spacing, please provide the estimated forest cover of you AOI in %."),
+
+          numericInput(ns("forest_cover"), "Forest cover in %", value = 50, min = 0, max = 100)
 
           )), ## END wellPanel step 1
 
@@ -88,13 +134,27 @@ params_UI <- function(id){
 
 
 
-    ))) ## End params_setup
+    ))), ## End params_setup
 
 
 
+    ## Show params quick consequences #######################################
 
+    shinyjs::hidden(
+      tabsetPanel(
+        id = ns("params_show"),
 
+        tabPanel("CV"),
+        tabPanel("Nested plot"),
+        tabPanel("Unit times"),
+        tabPanel("Optimization")
 
+      ),
+
+      ## !!! TO BE REMOVED !!!
+      p("!!! Placeholder for showing info based on selected parameters !!!")
+
+    ) ## End params show
 
   ) ## END tagList
 
--- R/params_server.R
+++ R/params_server.R
@@ -6,15 +6,45 @@ params_server <- function(id, rv) {
     ## Below is the module function
     function(input, output, session) {
 
+      ns <- session$ns
+
+      ## !!! For testing only
+      output$test_approach <- renderText({ paste0("Approach selected: ", rv$CV_model$cv_approach) })
+
+      output$test_CV <- renderTable({ rv$CV_model$cv_mixed })
+
+
+
       ## Check if CV model approach selected ################################
       observe({
 
-        if (is.null(rv$CV_model$cv_approach) | is.null(rv$CV_model$cv_mixed)) {
-          shinyjs::show("check_approach")
-          shinyjs::hide("params_setup")
-        } else {
+        req(rv$CV_model$cv_approach)
+
+        if (rv$CV_model$cv_approach == "a2") {
           shinyjs::hide("check_approach")
           shinyjs::show("params_setup")
+          shinyjs::show("params_a2")
+        } else {
+          shinyjs::show("check_approach")
+          shinyjs::hide("params_setup")
+          shinyjs::hide("params_a2")
+        }
+
+      })
+
+      observe({
+
+        req(rv$CV_model$cv_approach)
+        req(rv$CV_model$cv_mixed)
+
+        if (rv$CV_model$cv_approach == "a1") {
+          shinyjs::hide("check_approach")
+          shinyjs::show("params_setup")
+          shinyjs::show("params_a1")
+        } else {
+          shinyjs::show("check_approach")
+          shinyjs::hide("params_setup")
+          shinyjs::hide("params_a2")
         }
 
       })
@@ -22,6 +52,6 @@ params_server <- function(id, rv) {
       ## box 1: CV model ####################################################
 
 
-    } ## END module function
+    } ## END module server function
   )
 } ## END function params_server()
