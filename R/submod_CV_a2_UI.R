02cbd5fd4a8a93877cc98eb827668ea3df69cceb
gaelso-bkp <gaso.ella@gmail.com>
2022-09-20 00:52:47 +0900

    cosmetic changes for additional level 2 subplot influence on CV

--- R/submod_CV_a2_UI.R
+++ R/submod_CV_a2_UI.R
@@ -18,58 +18,59 @@ submod_CV_a2_UI <- function(id){
         $a_{1}$ and $a_{2}$ the subplot size in ha for level 1 and 2 of nested subplots,
         and $\\beta_{0} ... \\beta_{4}$ the model's parameters."),
 
-    sidebarLayout(
 
+    ## + User inputs ====================================================
 
+    wellPanel(
 
-      ## + User inputs ====================================================
+      h4("Fill in the model parameters:"),
 
-      sidebarPanel(
+      p("The CV model parameters are pre-filled with default values from a Forest
+        Inventory in the tropics. They are intended for testing purposes and should
+        be replaced with values from your area of interest or similar conditions.",
+        style = "color: #17a2b8; font-style: italic;"), ## bootstrap info color
 
-        h4("Fill in the model parameters:"),
+      fluidRow(
 
-        p("The CV model parameters are pre-filled with default values from a Forest
-          Inventory in the tropics. They are intended for testing purposes and should
-          be replaced with values from your area of interest or similar conditions.",
-          style = "color: #17a2b8; font-style: italic;"), ## bootstrap info color
+        column(2, offset = 1, numericInput(inputId = ns("beta0"), label = "$\\beta_{0}$",
+                               value = 1.02, min = 0, max = 10, step = 0.001)),
 
-        fluidRow(
+        column(2, numericInput(inputId = ns("beta1"), label = "$\\beta_{1}$",
+                               value = -0.15, min = -1, max = 1, step = 0.001)),
 
-          column(2, numericInput(inputId = ns("beta0"), label = "$\\beta_{0}$",
-                                 value = 1.02, min = 0, max = 10, step = 0.001)),
+        column(2, numericInput(inputId = ns("beta2"), label = "$\\beta_{2}$",
+                               value = 0.016, min = -0.1, max = 0.1, step = 0.001)),
 
-          column(2, numericInput(inputId = ns("beta1"), label = "$\\beta_{1}$",
-                                 value = -0.15, min = -1, max = 1, step = 0.001)),
+        column(2, numericInput(inputId = ns("beta3"), label = "$\\beta_{3}$",
+                               value = -0.12, min = -1, max = 1, step = 0.001)),
 
-          column(2, numericInput(inputId = ns("beta2"), label = "$\\beta_{2}$",
-                                 value = 0.016, min = -0.1, max = 0.1, step = 0.001)),
-
-          column(3, numericInput(inputId = ns("beta3"), label = "$\\beta_{3}$",
-                                 value = -0.12, min = -1, max = 1, step = 0.001)),
-
-          column(3, numericInput(inputId = ns("beta4"), label = "$\\beta_{4}$",
-                                 value = 0, min = -1, max = 1, step = 0.001))
+        column(2, numericInput(inputId = ns("beta4"), label = "$\\beta_{4}$",
+                               value = 0, min = -1, max = 1, step = 0.001))
 
         ),
 
-        width = 8),
+      ),
 
 
-      ## + Model test results =============================================
+    ## + Model test results =============================================
 
-      mainPanel(
+    fluidRow(
 
+      column(6, div(
         p("This table recaps where default parameters are used:"),
 
-        tableOutput(outputId = ns("a2_check")),
+        tableOutput(outputId = ns("a2_check"))
 
+      )),
+
+      column(6, div(
         p(strong("Parameters test:")),
 
-        textOutput(outputId = ns("a2_test")),
+        textOutput(outputId = ns("a2_test"))
 
-        width = 4) ## End Mainpanel A2 results
+      ))
 
-    ) ## END sidebar layout
+    )
 
   )
 }
